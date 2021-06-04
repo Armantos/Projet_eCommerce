7998a92dc5dccd7c15135c60e0a8d932420461c8
HURAULT Armand <armandhurault94@gmail.com>
2021-06-05 00:40:40 +0200

    affichage details page article

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,30 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'home')]
     public function index(): Response
     {
-        return $this->render("/home/index.html.twig");
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $repo->findAll();
+
+        //dd($articles); //debug pour afficher le tableau d'articles
+
+        return $this->render("/home/index.html.twig",
+            ['articles' => $articles,
+            ]);
+    }
+
+    #[Route('/show/{id}', name: 'show')]
+    public function show($id): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $article = $repo->find($id);
+
+        if (!$article) { //redirige vers la page home si l'id de l'article n'est pas trouve
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render("show/index.html.twig",
+            ['article' => $article,
+            ]);
     }
 }
