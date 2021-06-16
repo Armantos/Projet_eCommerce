d3320e78ce72f225f1576594e8dcf735e326aff0
HURAULT Armand <armandhurault94@gmail.com>
2021-06-16 15:57:27 +0200

    empeche la validation de la commande si l'utilisateur n'est pas connecte

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -12,7 +12,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
+    #[Route('/register', name: 'register')]
     public function register(Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
